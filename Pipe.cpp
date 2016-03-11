e8df4094aea78c391e1b4b7b8e81d43cf608aceb
ggeschw <ggeschw@URB218-32.seasad.wustl.edu>
2016-03-10 20:58:04 -0600

    Added new Pipe class for piping

---
+++ Pipe.cpp
@@ -0,0 +1,18 @@
+/*
+ * Pipe.cpp
+ *
+ *  Created on: Mar 10, 2016
+ *      Author: ggeschw
+ */
+
+#include "Pipe.h"
+
+Pipe::Pipe() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Pipe::~Pipe() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Pipe.h
@@ -0,0 +1,17 @@
+/*
+ * Pipe.h
+ *
+ *  Created on: Mar 10, 2016
+ *      Author: ggeschw
+ */
+
+#ifndef PIPE_H_
+#define PIPE_H_
+
+class Pipe {
+public:
+	Pipe();
+	virtual ~Pipe();
+};
+
+#endif /* PIPE_H_ */
